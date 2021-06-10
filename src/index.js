a08e62fcc86d1b8fabb506c08ef43f0fe91b66c2
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-06-10 18:46:21 +0300

    Students modal

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './sass/main.scss';
 import MovieApiService from './js/movieApiService';
 import getMovieListWithGenresName from './js/getGenresName';
 import { renderMovieList } from './js/renderMarckup';
+import modalStudents from './js/students-modal';
 
 export const movieApiService = new MovieApiService();
 
@@ -10,3 +11,5 @@ movieApiService.fetchTrendingMovies()
   .then(getMovieListWithGenresName)
   .then(renderMovieList)
   .catch(error => console.log(error ));
+
+  modalStudents();

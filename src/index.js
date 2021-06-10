4e53bc27e3f0045ca904519f192b3a5858bd9c31
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-06-10 20:05:21 +0300

    Добавил рендер хедера по шаблону

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './sass/main.scss';
+import homePageHeaderMarckup from './templates/home-page-header.hbs'
 
 import MovieApiService from './js/movieApiService';
 import getMovieListWithGenresName from './js/getGenresName';
 
-import renderHomePageHeader from './js/homePageHeader';
-import { renderMovieList } from './js/renderMarckup';
+import { renderMovieList, renderHeader } from './js/renderMarckup';
 
 export const movieApiService = new MovieApiService();
+
+renderHeader(homePageHeaderMarckup);
 
 movieApiService.fetchTrendingMovies()
   .then(getMovieListWithGenresName)

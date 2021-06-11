5bac16c886d082efe36ccfdc6dca4eb874c0c93e
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-06-11 21:43:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './sass/main.scss';
 
-import homePageHeaderMarckup from './templates/home-page-header.hbs'
+import homePageHeaderMarckup from './templates/home-page-header.hbs';
+import libraryHeaderMarckup from './templates/library-header.hbs';
 import MovieApiService from './js/movieApiService';
 import getMovieListWithGenresName from './js/getGenresName';
 import { renderMovieList, renderHeader } from './js/renderMarckup';
-import modalStudents from './js/students-modal';
+import './js/students-modal';
 
 export const movieApiService = new MovieApiService();
 
@@ -14,5 +15,3 @@ movieApiService.fetchTrendingMovies()
   .then(getMovieListWithGenresName)
   .then(renderMovieList)
   .catch(error => console.log(error ));
-
-modalStudents();

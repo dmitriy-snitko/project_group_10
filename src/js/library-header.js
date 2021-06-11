7d45ad854f88f99b4b8e56d38133d6aab61af963
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-06-11 21:43:40 +0300

    Update library-header.js

--- src/js/library-header.js
+++ src/js/library-header.js
@@ -1,30 +1,30 @@
-import LibraryHederTemplate from '../templates/library-header.hbs';
+// import LibraryHederTemplate from '../templates/library-header.hbs';
 
-const refs = {
+// const refs = {
 
-    navLink: document.querySelector(".nav-link"),
-    navItem: document.querySelector(".nav-item"),
-    libraryLink: document.querySelector('#library-link'),
-    libraryLogo: document.querySelector(".navigation-logo"),
-    libraryLink: document.querySelector('#library-link'),
-    libraryPage: document.querySelector('.library-page-header-container'),
-    libraryBody: document.querySelector('body')
-}
+//     navLink: document.querySelector(".nav-link"),
+//     navItem: document.querySelector(".nav-item"),
+//     libraryLink: document.querySelector('#library-link'),
+//     libraryLogo: document.querySelector(".navigation-logo"),
+//     libraryLink: document.querySelector('#library-link'),
+//     libraryPage: document.querySelector('.library-page-header-container'),
+//     libraryBody: document.querySelector('body')
+// }
 
-refs.libraryLink.addEventListener('click', () => {
-   e.preventDefault();
+// refs.libraryLink.addEventListener('click', () => {
+//    e.preventDefault();
     
-    refs.libraryPage.textContent = "перешли на страни" 
+//     refs.libraryPage.textContent = "перешли на страни" 
     
-})
-// function renderLibraryHeader(e) {
-// refs.libraryLogo.addEventListener('click', renderLibraryHeader);
-// refs.libraryLink.addEventListener('click', renderLibraryHeader);
+// })
+// // function renderLibraryHeader(e) {
+// // refs.libraryLogo.addEventListener('click', renderLibraryHeader);
+// // refs.libraryLink.addEventListener('click', renderLibraryHeader);
 
-// function renderLibraryHeader(e) {
-//     e.preventDefault();
-//     refs.libraryBody.innerHTML = LibraryHederTemplate();
+// // function renderLibraryHeader(e) {
+// //     e.preventDefault();
+// //     refs.libraryBody.innerHTML = LibraryHederTemplate();
 
-// } 
+// // } 
 
 

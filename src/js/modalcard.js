aaed29c4d935e0874b4ac1a2ee887c1621259c7a
Anna <73854503+Blossom0204@users.noreply.github.com>
2021-06-12 20:59:41 +0300

    Правки

--- src/js/modalcard.js
+++ src/js/modalcard.js
@@ -1,5 +1,5 @@
-// import modalWindowTpl from '../templates/modalcard.hbs';
-// import { API_KEY } from './movieApiService';
+import modalTpl from '../templates/modalcard.hbs';
+import API_KEY from './movieApiService';
 
 const refs = {
   modal: document.querySelector('.modal'),
@@ -15,13 +15,42 @@ function closeModal() {
   refs.body.classList.remove('hide-overflow');
 }
 refs.btnClose.addEventListener('click', closeModal);
-refs.backdropmodal.addEventListener('click', e => {
-  if (e.target === e.currentTarget) {
+refs.backdropmodal.addEventListener('click', evt => {
+  if (evt.target === evt.currentTarget) {
     closeModal();
   }
 });
-window.addEventListener('keydown', e => {
-  if (e.code === 'Escape' && !refs.backdropmodal.classList.contains('is-hidden')) {
-    closeModal();
-  }
-});
+// window.addEventListener('keydown', evt => {
+//   if (evt.code === 'Escape' && !refs.backdropmodal.classList.contains('is-hidden')) {
+//     closeModal();
+//   }
+// });
+
+// refs.gallery.addEventListener('click', getCard);
+
+// function getCard(event) {
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+//   const movieId = event.target.dataset.id;
+//   modalLoad(movieId);
+//   openModal();
+// }
+// function getMovieById(id) {
+//   return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`).then(response =>
+//     response.json(),
+//   );
+// }
+
+// function modalLoad(movieId) {
+//   getMovieById(id).then(evt => {
+//     updateModalMarkup(evt);
+//   });
+// }
+// function updateModalMarkup(evt) {
+//   const modalMarkup = modalTpl(evt);
+//   refs.modal.insertAdjacentHTML('beforeend', modalMarkup);
+// }
+// function openModal() {
+//   refs.backdropmodal.classList.add('is-open');
+// }

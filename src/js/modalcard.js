5eb157a106f614b9536dd34193430509a248ca34
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-06-14 22:27:57 +0300

    Update modalcard.js

--- src/js/modalcard.js
+++ src/js/modalcard.js
@@ -5,29 +5,34 @@ import { movieApiService } from '../index';
 function openModal() {
   refs.backdropmodal.classList.remove('is-hidden');
   refs.backdropmodal.classList.add('is-open');
-}
+
+};
+
 export function closeModal() {
   refs.backdropmodal.classList.remove('is-open');
   refs.backdropmodal.classList.add('is-hidden');
   refs.modalBox.innerHTML = '';
-  // refs.body.classList.remove('hide-overflow');
-}
+};
 
 export function getCard(event) {
+  event.preventDefault();
+
   if (event.target.nodeName !== 'IMG') {
     return;
-  }
+  };
+
   const movieId = event.target.dataset.id;
   modalLoad(movieId);
   openModal();
-}
+};
 
 function modalLoad(id) {
   movieApiService.fetchMoviesById(id).then(evt => {
     updateModalMarkup(evt);
   });
-}
+};
+
 function updateModalMarkup(evt) {
   const modalMarkup = modalTpl(evt);
   refs.modal.innerHTML = modalMarkup;
-}
+};

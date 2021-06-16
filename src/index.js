e5dc3d36fff2d12ae4b26707d31f3ee96dfb46a1
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-06-16 20:27:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { getCard, closeModal } from './js/modalcard';
 import { onPageHeaderClick } from './js/navigation';
 import { onSearch } from './js/search-movie';
 import './js/students-modal';
-import 'animate.css';
+// import 'animate.css';
 
 
 export const movieApiService = new MovieApiService();
@@ -25,6 +25,7 @@ refs.backdropmodal.addEventListener('click', evt => {
     closeModal();
   }
 });
+
 window.addEventListener('keydown', evt => {
   if (evt.code === 'Escape' && !refs.backdropmodal.classList.contains('is-hidden')) {
     closeModal();
